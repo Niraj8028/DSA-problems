7eb48acbc7786eada5436ee03622aafe677692e5
Niraj8028 <nirajpardeshi2002@gmail.com>
2022-03-02 11:32:10 +0530

    revision

--- Graph/revision.cpp
+++ Graph/revision.cpp
@@ -1,39 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
-
-void WaterJug(int x,int y,int z){
-    vector<int>vis(x+y,0);
-    queue<int>q;
-    q.push(0);
-    vis[0]=1;
-    int dir[]={x,-x,y,-y};
-    while(!q.empty()){
-        int t=q.front();
-        vis[t]=1;
-        q.pop();
-        if(t==z){
-            return;
+#define vi vector<int>
+#define pii vector<pair<int,int>>
+vi indegree;
+vi outdegree;
+void Dfs(vi adj[],node,vi &vis,int timer){
+    vis[node]=1;
+    indegree[node]=timer++;
+    for(auto it: adj[node]){
+        if(!vis[it]){
+            dfs(adj,it,vis,timer);
         }
-        for(int i=0; i<4;i++){
-            int water=t+dir[i];
-            if(water<=0 || water>=x+y){
-                continue;
-            }
-            if(water==z){
-                return;
-            }
-            if(!vis[water]){
-                q.push(water);
-                vis[water]=1;
-            }
-        }
+    }
+    outdegree[node]=timer++;
+}
 
-
+bool check(int x,int y){
+    if(indegree[y]>indegree[x] && outdegree[x]>outdegree[y]){
+        return true;
     }
 }
+
 int main(){
-    
+    int v,e;
+    vector<vector<int>>adj(v);
+    for(int i=0; i<e; i++){
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    dfs(0,timer,adj);
+    int q;
+    cin>>q;
+    for(int i=0; i<q; i++){
+        int type,x,y;
+        cin>>type>>x>>y;
+        if(!check(x,y) && !(check(y,x))){
+            cout<<"NO";
+            continue;
+        }
+        if(type==0){
+            if(check(y,x)){
+                cout<<"yes";
+            }
+            cout<<"no";
+        }
+        else if(type==1){
+            if(check(x,y)){
+                cout<<"yes";
+
+            }
+            cout<<"no";
+        }
+    }  
     return 0;
 }
