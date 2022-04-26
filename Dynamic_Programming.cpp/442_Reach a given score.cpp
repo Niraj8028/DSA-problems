d7d4f9d985814abc0d15e1e93983d17d50e0a782
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-04-26 11:39:06 +0530

    DP

---
+++ Dynamic_Programming.cpp/442_Reach a given score.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+// Driver Code
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ // } Driver Code Ends
+// Complete this function
+long long int count(long long int n)
+{
+	long long int table[n+1],i;
+	memset(table, 0, sizeof(table));
+	table[0]=1;                
+	for(int i=3; i<=n; i++){
+	    table[i]+=table[i-3];
+	}
+	for(int i=5; i<=n; i++){
+	    table[i]+=table[i-5];
+	}
+	for(int i=10; i<=n; i++){
+	    table[i]+=table[i-10];
+	}
+	return table[n];
+}
+
+// { Driver Code Starts.
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		cout<<count(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
