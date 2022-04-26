fd70153f11c65bb073905327a305b07c41dd6d53
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-04-26 13:03:36 +0530

    DP

---
+++ Dynamic_Programming.cpp/445_smallest_sum_conti_subarray.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+      //Code here
+      int sf=a[0];
+      int seh=a[0];
+      for(int i=1; i<a.size(); i++){
+          seh=min(seh+a[i],a[i]);
+          if(sf>seh){
+              sf=seh;
+          }
+      }
+      return sf;
+  }
+};
+
+
+// { Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        Solution ob;
+        
+        int ans = ob.smallestSumSubarray(a);
+        
+        cout<<ans<<endl;
+    }
+}  // } Driver Code Ends
