b301628f3dad9d48aba6e5d327ec344c959c9f3d
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-07 07:24:29 +0530

    DP

---
+++ Dynamic_Programming.cpp/437_max_diff_1_n_0.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int maxSubstring(string s)
+	{
+	    int sweep = 0, ans = -1;
+    for (auto c : s) {
+        if (c == '0')sweep++;
+        else sweep--;
+        ans = max(ans, sweep);
+        if (sweep < 0)sweep = 0;
+    }
+    return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.maxSubstring(s) << "\n";
+   	}
+
+    return 0;
+}
+  // } Driver Code Ends
