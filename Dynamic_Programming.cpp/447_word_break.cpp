e4ba670b82f5a3670c05a3dda6d61c2f0819664b
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-09 07:38:41 +0530

    DP

--- Dynamic_Programming.cpp/447_word_break.cpp
+++ Dynamic_Programming.cpp/447_word_break.cpp
@@ -15,38 +15,24 @@ class Solution
 {
 public:
     int wordBreak(string s, vector<string> &B) {
-        int n=s.size();
-        bool dp[n+1][n+1];
-        unordered_set<string>set;
-        for(auto it:B){
-            set.insert(it);
-        }
-        
-        
-        for(int i=1; i<=n; i++){
-            int start=1;
-            int end=start+i-1;
-            while(end<=n){
-                string temp=s.substr(start,i);
-                if(set.find(temp)!=set.end()){
-                    dp[start][end]=1;
-                }
-                else{
-                    bool flag=false;
-                    for(int i=start; i<end; i++){
-                        if(dp[start][i] && dp[i+1][end]){
-                            flag=true;
-                            break;
-                        }
-                        
-                    }
-                    dp[start][end]=flag;
-                }
-                start++;
-            }
-            
-        }
-        return dp[1][n];
+        int wordBreak(string a, vector<string> &b) {
+        int n=a.size();
+       unordered_set<string> s;
+       vector<bool> dp(n+1,false);
+       for(int i=0;i<b.size();i++){
+           s.insert(b[i]);
+       }
+       dp[0]=true;
+       for(int i=1;i<=n;i++){
+           for(int j=i-1;j>=0;j--){
+              if(dp[j]==true &&
+              s.find(a.substr(j,i-j))!=s.end()){
+                dp[i]=true;
+                break;
+              }
+           }
+       }
+       return dp[n];
     }
 };
 
