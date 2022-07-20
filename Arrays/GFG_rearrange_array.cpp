0f407c9357711e4ea5c5d25652c91b16317378db
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-07-20 07:24:22 +0530

    GFG

---
+++ Arrays/GFG_rearrange_array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+      
+        Rearrange( arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
+
+
+
+
+void Rearrange(int arr[], int n)
+{
+    // Your code goes here
+    int i=-1;
+    for(int j=0; j<n; j++){
+        if(arr[j]<0){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+}
