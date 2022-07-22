a4af52062e6f78b0968f0a50713d0f6e555b9f65
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-07-22 08:37:37 +0530

    GFG

---
+++ Arrays/GFG_move_neg_ele_end_array.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+        vector<int>pos;
+        vector<int>neg;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i]>=0){
+                pos.push_back(arr[i]);
+            }
+            else{
+                neg.push_back(arr[i]);
+            }
+        }
+        int m=pos.size();
+        
+        for(int i=0; i<m; i++){
+            arr[i]=pos[i];
+        }
+        int l=neg.size();
+        
+        for(int i=0; i<l; i++){
+            arr[m+i]=neg[i];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
+}
+
+// } Driver Code Ends
