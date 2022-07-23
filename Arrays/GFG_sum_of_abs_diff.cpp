91bccfe050e9d755ac74476d3b685eaf4f6a9df8
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-07-23 09:12:48 +0530

    GFG

---
+++ Arrays/GFG_sum_of_abs_diff.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long findMinSum(vector<int> &A,vector<int> &B,int N){
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        
+        long long sum=0;
+        for(int i=0; i<N; i++){
+            long long diff=abs(A[i]-B[i]);
+            sum+=diff;
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> A(N),B(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        for(int i=0;i<N;i++){
+            cin>>B[i];
+        }
+        Solution ob;
+        cout<<ob.findMinSum(A,B,N)<<endl;
+    }
+}
+// } Driver Code Ends
