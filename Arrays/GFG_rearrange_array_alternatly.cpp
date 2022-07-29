7c508c4413a00bec341cbb79e226d95ef618f5b2
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-07-29 10:02:37 +0530

    GFG

---
+++ Arrays/GFG_rearrange_array_alternatly.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+// C++ program to rearrange an array in minimum 
+// maximum form 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // This function wants you to modify the given input
+    // array and no need to return anything
+    // arr: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    void rearrange(long long *arr, int n) 
+    { 
+    	
+    	// Your code here
+    	long long mx=arr[0];
+    	for(int i=1; i<n; i++){
+    	    mx=max(mx,arr[i]);
+    	}
+    	int me=mx+1;
+    	int i=0;
+    	int start=0;
+    	int end=n-1;
+    	while(start<=end){
+    	    if(i%2==0){
+    	        arr[i]=arr[i]+((arr[end]%me)*me);
+    	        i++;
+    	        end--;
+    	    }
+    	    else{
+    	        arr[i]=arr[i]+((arr[start]%me)*me);
+    	        i++;
+    	        start++;
+    	    }
+    	}
+    	for(int i=0; i<n; i++){
+    	    arr[i]=arr[i]/me;
+    	}
+    	
+    	 
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above function 
+int main() 
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        
+        //size of array
+        int n;
+        cin >> n;
+        
+        long long arr[n];
+        
+        //adding elements to the array
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        
+        //calling rearrange() function
+        ob.rearrange(arr, n);
+        
+        //printing the elements
+        for (int i = 0; i < n; i++) 
+		    cout << arr[i] << " ";
+		
+		cout << endl;
+    }
+	return 0; 
+} 
+
+// } Driver Code Ends
