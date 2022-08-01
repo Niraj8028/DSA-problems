3bf0a02257611b95eb7a5ea26fb68ad317f66b65
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-01 14:59:42 +0530

    GFG

---
+++ Arrays/GFG_group_anagrams_together.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& arr) {
+        //code here
+        unordered_map<string, vector<string>>mp;
+        int n=arr.size();
+        
+        for(int i=0; i<n; i++){
+            string temp=arr[i];
+            sort(arr[i].begin(),arr[i].end());
+            mp[arr[i]].push_back(temp);
+        }
+        vector<vector<string>>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
