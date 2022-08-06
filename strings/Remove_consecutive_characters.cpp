6a5f837a40c3edef96ea19dff10747677a0e1aa1
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-06 08:58:54 +0530

    GFG

---
+++ strings/Remove_consecutive_characters.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+        // code here.
+        string ans="";
+        for(int i=0; i<S.size(); i++){
+            if(S[i]==S[i+1]){
+                continue;
+            }
+            ans+=S[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
