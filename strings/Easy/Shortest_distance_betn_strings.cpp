58c29830e7aaa5e3d700e971cd59454dd976cceb
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-06 15:45:41 +0530

    GFG

---
+++ strings/Easy/Shortest_distance_betn_strings.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int shortestDistance(vector<string> &s, string word1, string word2)
+	{
+		// Your code goes here
+		 int f=0,ss=0;
+          int ans=INT_MAX;
+          bool first=false,sec=false;
+ 
+         for(int i=0;i<s.size();i++){
+             if(s[i]==word1){
+                  f=i;
+                  first=1;
+
+         }
+         if(s[i]==word2){
+             ss=i;
+             sec=1;
+         }
+        if( first and sec){
+            ans=min(ans,abs(f-ss));
+          }
+    }
+   return ans;
+		
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		int n;
+   		cin >> n;
+
+   		vector<string> s(n);
+   		for(int i = 0; i < n; i++)
+   			cin >> s[i];
+
+   		string word1, word2;
+
+   		cin >> word1 >> word2;
+   		
+   		Solution ob;
+
+   		cout << ob.shortestDistance(s, word1, word2) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
