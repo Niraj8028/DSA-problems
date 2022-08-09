0297a042e3956a477bde815c988062f479954ff0
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-09 11:08:14 +0530

    GFG

---
+++ strings/Easy/longest_repeating_subsequence.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+	int dp[1001][1001];
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    int n=str.length();
+		    for(int i=0; i<=n; i++){
+		        for(int j=0; j<=n; j++){
+		            if(i==0 || j==0){
+		                dp[i][j]=0;
+		            }
+		        }
+		    }
+		    for(int i=1; i<=n; i++){
+		        for(int j=1; j<=n; j++){
+		            if(str[i-1]==str[j-1] && (i!=j)){
+		                dp[i][j]=1+dp[i-1][j-1];
+		            }
+		            else{
+		                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+		            }
+		        }
+		    }
+		    int ans=0;
+		    for(int i=1; i<=n; i++){
+		        for(int j=1; j<=n; j++){
+		            ans=max(ans,dp[i][j]);
+		        }
+		    }
+		    return ans;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.LongestRepeatingSubsequence(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
