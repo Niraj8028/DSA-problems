c62b03d86e88afb6261a9986be97ed0581d64af2
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-25 08:07:15 +0530

    GFG

---
+++ strings/Easy/test.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define vi vector<int>
+#define pii vector<pair<int,int>>
+int main(){
+    int n=5;
+cnt=1;
+    int arr[n]={3,1,2,2,2};
+    1 3
+    4 2 
+
+    return 0;
+}
