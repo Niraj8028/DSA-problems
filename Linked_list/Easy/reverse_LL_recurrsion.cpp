ea425489bb02580a72cfce56a2a0394d8c44a417
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-28 13:53:02 +0530

    GFG

---
+++ Linked_list/Easy/reverse_LL_recurrsion.cpp
@@ -0,0 +1,88 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    void reverse(Node* &head,Node*curr, Node*prev){
+        if(curr==NULL){
+            head=prev;
+            return;
+        }
+        reverse(head,curr->next,curr);
+        curr->next=prev;
+    }
+    
+    struct Node* reverseList(struct Node *head)
+    {
+        Node*curr=head;
+        Node*prev=NULL;
+        reverse(head,curr,prev);
+        return head;
+    }
+    
+};
+    
+
+
+//{ Driver Code Starts.
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        head = ob. reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
