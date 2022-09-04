e229a295a124edd464fb6e65f35cbd7102d549d7
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-04 19:06:51 +0530

    GFG

---
+++ Linked_list/Medium/count_triplets.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+ 
+/* Link list node */
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void push(struct Node** head_ref, int new_data)
+{
+   
+    struct Node* new_node = new Node(new_data);
+    
+    new_node->next = (*head_ref);
+    (*head_ref)    = new_node;
+} 
+
+int countTriplets(struct Node* head, int x) ;
+
+/* Driver program to test count function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n , x ,i , num;
+        struct Node *head = NULL;
+        cin>>n>>x;
+        for(i=0;i<n;i++)
+        {
+            cin>>num;
+            push(&head,num);
+        }
+ 
+    /* Check the count function */
+    cout <<countTriplets(head, x)<< endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+int countTriplets(struct Node* head, int x) 
+{ 
+    // code here.
+    unordered_map<int,int>mp;
+    Node*temp=head;
+    int cnt=0;
+    int sum=x;
+    while(temp->next!=NULL){
+        sum=sum-temp->data;
+        Node*node=temp->next;
+        while(node!=NULL){
+            if(mp.find(sum-node->data)!=mp.end()){
+                cnt++;
+                
+            }
+            
+            node=node->next;
+        }
+        mp[temp->data]++;
+        temp=temp->next;
+    }
+    return cnt;
+} 
