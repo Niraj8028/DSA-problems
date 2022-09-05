78c7b55a936abe7c2a465b0326ce88ab8b121026
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-05 07:27:05 +0530

    GFG

---
+++ Sliding_window_technique.cpp/max_of_subarray_size_k.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int k, vector<int> &Arr , int n){
+        // code here 
+        long ans=0;
+        
+        long sum=0;
+        for(int i=0; i<k; i++){
+            sum+=Arr[i];
+        }
+        ans=max(ans,sum);
+        int j=0;
+        for(int i=k; i<n; i++){
+            
+            sum+=Arr[i]-Arr[j];
+            j++;
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
