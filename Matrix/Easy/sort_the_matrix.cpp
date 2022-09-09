207e591e4049f70fdb62d6577153dff0de554e9c
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-09 13:30:27 +0530

    GFG

---
+++ Matrix/Easy/sort_the_matrix.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int n, vector<vector<int>> Mat) {
+        // code here
+        vector<int>v;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                v.push_back(Mat[i][j]);
+            }
+        }
+        sort(v.begin(),v.end());
+        int k=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                Mat[i][j]=v[k];
+                k++;
+            }
+        }
+        return Mat;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> v(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> v[i][j];
+        Solution ob;
+        v = ob.sortedMatrix(N, v);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+// } Driver Code Ends
