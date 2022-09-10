1a853acc795e855311d35d3821c485a7f30953fc
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-10 09:19:32 +0530

    GFG

---
+++ Matrix/Easy/max_1S_in_row.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int maxOnes (vector <vector <int>> &mat, int n, int m)
+        {
+            // your code herefor
+            for(int i=0; i<m; i++){
+                for(int j=0; j<n; j++){
+                    if(mat[j][i]==1){
+                        return j;
+                    }
+                }
+            }
+        }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+        int n, m; cin >> n >> m;
+        vector <vector <int>> arr (n, vector <int> (m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> arr[i][j];
+        Solution ob;        
+        cout << ob.maxOnes(arr, n, m) << endl;
+    }
+}
+// } Driver Code Ends
