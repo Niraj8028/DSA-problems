47bd4f654de6a6a0fe6d00570ddc0af022eaadd9
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-21 12:35:05 +0530

    GFG

---
+++ Stack_and_queue/Medium/Sort_the_stack.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+
+void sortST(stack<int>&s,int x){
+    if(s.empty() || s.top()<=x){
+        s.push(x);
+        return;
+    }
+    int y=s.top();
+    s.pop();
+    sortST(s,x);
+    s.push(y);
+    return;
+    
+}
+
+void sortStack(stack<int>&s){
+    if(s.empty()){
+        return;
+    }
+    int x=s.top();
+    s.pop();
+    sortStack(s);
+    sortST(s,x);
+    
+}
+
+
+void SortedStack :: sort()
+{
+   //Your code here
+   sortStack(s);
+   
+   
+}
