7ecd89ac76ed1c053363196ebbaef67c83ea413a
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-21 11:32:44 +0530

    test

--- strings/Easy/test.cpp
+++ strings/Easy/test.cpp
@@ -2,12 +2,31 @@
 using namespace std;
 #define vi vector<int>
 #define pii vector<pair<int,int>>
-int main(){
-    int n=5;
-cnt=1;
-    int arr[n]={3,1,2,2,2};
-    1 3
-    4 2 
 
-    return 0;
+int getSum(int n){
+    int sum = 0;
+    while(n!=0) {
+        sum=sum+n%10;
+        n=n/10;
+    }
+    return sum;
+}
+
+vector<int>prodDelivery(vector<int>orderID){
+    vector<int>ans;
+    for(int i=0; i<orderID.size(); i++){
+        int x=getSum(orderID[i]);
+        ans.push_back(x);
+    }
+    return ans;
+
+}
+
+int main(){
+    vector<int>arr={43,345,20,987};
+    vector<int>ans=prodDelivery(arr);
+    for(int i=0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+
 }
