ca23f924d3bcbf67906e9c09c9a7640ca340c151
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-11-09 08:04:19 +0530

    GFG

--- strings/Easy/longest_distinct_char_string.cpp
+++ strings/Easy/longest_distinct_char_string.cpp
@@ -1,39 +1,14 @@
 //{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
-int longestSubstrDistinctChars (string S);
-int main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        string S; cin >> S;
 
-        cout << longestSubstrDistinctChars (S) << endl;
-    }
+int main(){
+    int *parr,len=10;
+    parr=new int[len];
+    delete parr;
+    return 0;
 }
 
 // Contributed By: Pranay Bansal
 
 // } Driver Code Ends
-
-
-int longestSubstrDistinctChars (string S)
-{
-    // your code here
-    int len=0;
-    int l=0;
-    int r=0;
-   
-    vector<int>mp(256,-1);
-    while(r<S.length()){
-        
-        if(mp[S[r]]!=-1){
-            l=max(l,mp[S[r]]+1);
-        }
-        mp[S[r]]=r;
-        len=max(len,r-l+1);
-        r++;
-    }
-    return len;
-}
