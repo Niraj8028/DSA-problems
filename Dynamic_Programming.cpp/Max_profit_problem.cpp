72a2ea2212534f8541ccd2c8ffea4bd391d61e18
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-11-14 09:58:50 +0530

    problem solved

--- Dynamic_Programming.cpp/Max_profit_problem.cpp
+++ Dynamic_Programming.cpp/Max_profit_problem.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 #define vi vector<int>
 #define pii vector<pair<int,int>>
-int dp[1000][3];
+int dp[1000][4];
 
 
-int solve(int time[3],int profit[3],int n){
+int solve(int time[3],int profit[3],int n,vector<int>&x){
     if(n<=4){
         return 0;
     }
     for(int i=0; i<=4; i++){
-        for(int j=0; j<3; j++){
+        for(int j=0; j<4; j++){
             dp[i][j]=0;
         }
     }
@@ -30,17 +30,27 @@ int solve(int time[3],int profit[3],int n){
     for(int i=0; i<3; i++){
         mx=max(mx,dp[n][i]);
     }
+    for(int i=0; i<3; i++){
+        if(dp[n][i]==mx){
+            x[i]++;
+        }
+    }
     return mx;
 }
 
 int main(){
     int time[3]={5,4,10};
     int profit[3]={1500,1000,3000};
-    int n=13;
+    int n;
+    cout<<"Please Enter Time :";
+    cin>>n;
     if(n<=4){
         cout<<"0";
     }
-    int ans= solve(time,profit,n);
-    cout<<"ans "<<ans;
+    vector<int>x(3,0);
+    int ans= solve(time,profit,n,x);
+    cout<<"Profit "<<ans<<endl;
+    cout<<"T:"<<x[0]<<"P:"<<x[1]<<"C:"<<x[2];
+    
     return 0;
 }
